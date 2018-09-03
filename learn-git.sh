0464290c71bec48131b09dad0c94ceb349f14f0e
chen1994 <913869524@qq.com>
2018-09-03 02:05:44 -0400

    update learn-git.sh

--- learn-git.sh
+++ learn-git.sh
@@ -56,8 +56,55 @@
     # git remote add origin git@github.com:chen1994/learngit.git
     只有GitHub上我的仓库的账户列表中有本地SSH Key时，本地才能推送
     关联后，远程库的名字就是origin（git的默认叫法，也可以自定义）
+    fnst没有git认证，所以用不了git@github.com，只能使用https：
+    # git config --list
+    core.repositoryformatversion=0
+    core.filemode=true
+    core.bare=false
+    core.logallrefupdates=true
+    remote.origin.url=git@github.com:chen1994/learngit.git
+    remote.origin.fetch=+refs/heads/*:refs/remotes/origin/*
+    # git config --unset remote.origin.url
+    # git config --unset remote.origin.fetch
+    # git config --list
+    core.repositoryformatversion=0
+    core.filemode=true
+    core.bare=false
+    core.logallrefupdates=true
+    # git remote add origin https://github.com/chen1994/learn-git.git
+    # git config --list
+    core.repositoryformatversion=0
+    core.filemode=true
+    core.bare=false
+    core.logallrefupdates=true
+    remote.origin.url=https://github.com/chen1994/learn-git.git
+    remote.origin.fetch=+refs/heads/*:refs/remotes/origin/*
 15. 将本地库的所有内容推送到远程库中
     # git push -u origin master
     第一次推送master分支，需要加上-u参数，git会将本地的master分支内容推送到远程新的master分支，并且将本地master分支和远程master分支关联，以后再推送就可以简化命令。
+    本地init之后，要先有commit的更新，再push推送到远程仓库
+    以上是现有本地库再有远程库时，如何关联远程库
+16. 先创建远程库，再从远程库clone
+    16.1. GitHub上创建仓库时勾选"Initialize this repository with a README"
+    16.2. # git clone https://github.com/chen1994/learn-git.git
+    git支持多种传输协议，其中git默认的git://使用ssh, 使用https协议是因为公司内部只开放了http端口，缺点就是速度慢且每次推送都必须要输入口令。
+17. 分支与主分支
+    可以创建属于自己的分支，推送到远程库，以免影响同时在使用主分支的小伙伴，之后等分支开发完毕，再与主分支合并
+    git将每次提交串成一条时间线，这条时间线再git里叫做主分支，即master分支。master指向最新提交，HEAD指向master。
+    创建一个新的分支dev，git会新建一个dev指针，指向master当前提交的位置，然后改变HEAD的指向，将HEAD指向dev，这样master分支不会变，以后对工作区的修改和提交，就是针对dev分支的了，每提交一次，dev指针前进一步，而master指针不变。
+    等到dev分支上的工作完成了，合并分支时，git就只需要将master指向dev当前的提交。
+    合并分支完成后，就可以删除dev分支，git的操作就只是将dev指针删掉就可以了。
+    17.1. 创建dev分支
+    # git checkout -b dev
+    Switched to a new branch 'dev'
+    创建的同时自动切换到dev分支，之后工作区的所有提交都会提交到dev分支
+    -b表示branch
+    17.2. 查看当前分支
+    # git branch
+    * dev
+      master
+    git branch 命令会列出所有分支，*号标识当前分支
+    17.3. 切换回master分支
+    # git checkout master
 
 
